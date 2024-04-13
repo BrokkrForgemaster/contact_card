e45ea828e588cbc9ab1318aa27bf6357d8b5465e
Chad-Powellv1 <chada.powell@outlook.com>
2024-04-13 10:16:03 -0400

    update

---
+++ brokkr/src/data.js
@@ -0,0 +1,14 @@
+export const organization = {
+    name: "House Wolf",
+    description: "The House Wolf is a clan-based organization composed of members from multiple backgrounds, united by a common creed, language, and code.  Known primarily as highly-effective explorers and defenders, the House Wolf has, at various points in its history, played a major role as legendary pioneers and protectors in the vast expanse of the Star Citizen universe.",
+    logo: "./assets/images/banner2.png",
+    join: "https://robertsspaceindustries.com/orgs/CUTTERWOLF",
+    web: "https://www.housewolf.co"
+};
+
+export const character = {
+    name: "Brokkr_Forgemaster",
+    role: "Hand of the Clan",
+    bio: "Brokkr, hailing from the icy expanse of a distant star system, is a name spoken in hushed tones amongst the halls of Mandalore. Known far and wide as the Forgemaster, Brokkr's origin is as enigmatic as the ethereal glow of his armors that protect warriors in the heat of battle.Brokkr honed his craft harnessing the raw elements of his homeland, much like the giants of old, he was a prodigy, crafting weapons and armor with mythical strength and elegance, binding the icy ferocity and celestial resilience of his environment into every piece.His prowess did not go unnoticed. Mandalore, a rising power in the interstellar battlefield, saw in Brokkr not just a blacksmith, but an artisan whose works were as much art as they were instruments of power. As armorer, Brokkr's legend grew; his armors were not just constructs of metal but were imbued with the soul of a warrior, each piece echoing the storms of his icy homeland and the celestial grace of the stars overhead.",
+    email: "brokkr_forgemaster@housewolf.co"
+};
