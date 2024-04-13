bf4ee938f0672e9638b7c8f38fd4dbf8b6839656
Chad-Powellv1 <chada.powell@outlook.com>
2024-04-13 02:43:40 -0400

    first commit

---
+++ contact_card/src/data.js
@@ -0,0 +1,14 @@
+export const organization = {
+    name: "House Wolf",
+    description: "The House Wolf is a clan-based organization composed of members from multiple backgrounds, united by a common creed, language, and code.  Known primarily as highly-effective explorers and defenders, the House Wolf has, at various points in its history, played a major role as legendary pioneers and protectors in the vast expanse of the Star Citizen universe.",
+    logo: "./assets/images/banner2.png",
+    join: "https://robertsspaceindustries.com/orgs/CUTTERWOLF",
+    web: "https://www.housewolf.co"
+};
+
+export const character = {
+    name: "character_name",
+    role: "character_rank",
+    bio: "character_bio",
+    email: "email"
+};
